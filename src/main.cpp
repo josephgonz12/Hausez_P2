3ed143ad3445d8f65248126e1799c08c2c27502c
Jacob <jsotunde@ufl.edu>
2025-11-03 15:20:01 -0500

    Updated House class declarations

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,15 @@ int main() {
     while (getline(house_file, line))
     {
         istringstream iss(line);
+        string junk, price, bed, bath, city, zip_code, square_feet;
+        getline(iss, junk, ',');
+        getline(iss, price, ',');
+        getline(iss, bed, ',');
+        getline(iss, bath, ',');
+        getline(iss, city, ',');
+        getline(iss, zip_code, ',');
+        getline(iss, square_feet, ',');
+
 
     }
 }
