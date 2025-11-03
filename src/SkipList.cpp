7603f9568cbed77d9b1a6fc95badb3e03d92f8ff
coolbillyjones <josephg5002@gmail.com>
2025-11-03 13:35:14 -0500

    .h base structure complete for SkipList

--- src/SkipList.cpp
+++ src/SkipList.cpp
@@ -1,1 +1,31 @@
 #include "SkipList.h"
+#include <cstdlib>
+#include <ctime>
+
+int SkipList::getRandLevel(int maxLevel) {
+    int level = 0;
+
+    while (rand() % 2 == 1 && level < maxLevel) {
+        level++;
+    }
+    return level;
+}
+
+void SkipList::insert(std::string state, int price){
+
+}
+
+std::vector<House> SkipList::getCheapest(int num) {
+
+}
+
+std::vector<House> SkipList::filterByState(std::string state) {
+
+}
+
+void SkipList::display() {
+    
+}
+
+
+
