bb707ea6a11e7044df6a752239ad0dca10d1b4d3
Jacob <jsotunde@ufl.edu>
2025-11-03 20:57:25 -0500

    insert apt msg here

--- src/main.cpp
+++ src/main.cpp
@@ -5,23 +5,28 @@
 #include "House.h"
 using namespace std;
 int main() {
-    std::cout << "Proj2" << std::endl;
+    std::cout << "Projtest" << std::endl;
     ifstream house_file("florida_real_estate.csv");
     string line;
-    vector<House> houses;
+    vector<House> houses_list;
     getline(house_file, line);
     while (getline(house_file, line))
     {
         istringstream iss(line);
-        string junk, price, bed, bath, city, zip_code, square_feet;
+        string junk, p, bed, bath, city, zip;
         getline(iss, junk, ',');
-        getline(iss, price, ',');
+        getline(iss, p, ',');
         getline(iss, bed, ',');
         getline(iss, bath, ',');
         getline(iss, city, ',');
-        getline(iss, zip_code, ',');
-        getline(iss, square_feet, ',');
-
-
+        getline(iss, zip, ',');
+        int price = std::stoi(p);
+        int beds = stoi(bed);
+        int baths = stoi(bath);
+        auto house = House(price, beds, baths, city, zip);
+        houses_list.push_back(house);
     }
+    house_file.close();
+    cout << "Welcome to Florida Real Estate Listings!" << endl;
+    cout << "We have " << houses_list.size() << " houses available." << endl
 }
