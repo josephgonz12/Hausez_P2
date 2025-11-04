cdf10058c485cfa9f5b72f8bc675bff2f289ef14
Jacob <jsotunde@ufl.edu>
2025-11-04 12:58:39 -0500

    added skiplist to .json, added preliminary options for main loop

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <algorithm>
 #include "House.h"
+#include "SkipList.h"
 using namespace std;
 int main() {
     std::cout << "Welcome to Florida Real Estate!" << std::endl;
@@ -11,6 +12,11 @@ int main() {
     string line;
     vector<House> houses;
     getline(house_file, line);
+    SkipList Skip;
+     int bed_filter, bath_filter, zip_filter = 0;
+    string city_filter = "";
+    bool running = true;
+    
     while (getline(house_file, line))
     {
         istringstream iss(line);
@@ -27,10 +33,46 @@ int main() {
         getline(iss, square_feet, ',');
         House temp = House(stoi(price), stoi(bed), stoi(bath), city, stoi(zip_code));
         houses.push_back(temp);
+        Skip.insert(city, stoi(price), stoi(bed), stoi(bath), stoi(zip_code));
     }
-   
-    std::cout <<"There are " << houses.size() << " houses available." << std::endl;
-   
-    int nums;
-    std::cin >> nums;
+    while (running) {
+        std::cout <<"There are " << houses.size() << " houses available." << std::endl;
+        std::cout << "Current Filters: " << std::endl;
+        std::cout << "City: " << (city_filter == "" ? "None" : city_filter) << std::endl;
+        std::cout << "Beds: " << (bed_filter == 0 ? "Any" : to_string(bed_filter)) << std::endl;
+        std::cout << "Baths: " << (bath_filter == 0 ? "Any" : to_string(bath_filter)) << std::endl;
+        std::cout << "Zip Code: " << (zip_filter == 0 ? "Any" : to_string(zip_filter)) << std::endl;    
+        std::cout << "0: Exit" << std::endl;
+        std::cout << "1: Set City" << std::endl;
+        std::cout << "2: Set Beds" << std::endl;
+        std::cout << "3: Set Baths" << std::endl;
+        std::cout << "4: Set Zip Code" << std::endl;
+        std::cout << "5: View Top 5 Cheapest Houses" << std::endl;
+        std::cout << "Select an option: ";
+        int option;
+        std::cin >> option;
+        if (option == 0){running = false;}
+        if (option == 1){
+            std::cout << "Enter City Name: ";
+            std::cin >> city_filter;
+        }
+        if (option == 2){
+            std::cout << "Enter Number of Beds: ";
+            std::cin >> bed_filter;
+        }
+        if (option == 3){
+            std::cout << "Enter Number of Baths: ";
+            std::cin >> bath_filter;
+        }
+        if (option == 4){
+            std::cout << "Enter Zip Code: ";
+            std::cin >> zip_filter;
+        }
+        if (option == 5){
+            auto cheapestHouses = Skip.getCheapest(5);
+            for (const auto& house : cheapestHouses) {
+                std::cout << "Price: " << house.price << ", Beds: " << house.beds << ", Baths: " << house.baths << ", City: " << house.city << ", Zip Code: " << house.zip_code << std::endl;
+            }
+        }
+    }
 }
