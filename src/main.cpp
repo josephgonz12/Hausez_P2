562ac93764ba234727572c45f62fe28ef3814e5a
Jacob <jsotunde@ufl.edu>
2025-11-04 14:01:35 -0500

    added basic implementation of skip list into main loop

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <algorithm>
 #include "House.h"
 #include "SkipList.h"
+#include <stdlib.h>
 using namespace std;
 int main() {
     std::cout << "Welcome to Florida Real Estate!" << std::endl;
@@ -13,16 +14,21 @@ int main() {
     vector<House> houses;
     getline(house_file, line);
     SkipList Skip;
-     int bed_filter, bath_filter, zip_filter = 0;
+    int bed_filter;
+    int bath_filter;
+    int zip_filter;
     string city_filter = "";
     bool running = true;
     
     while (getline(house_file, line))
     {
+        
         istringstream iss(line);
         string junk, price, bed, bath, city, state, zip_code, square_feet;
         getline(iss, junk, ',');
         getline(iss, price, ',');
+        double val = stod(price);
+        price = to_string(static_cast<int>(val));
         getline(iss, bed, ',');
         getline(iss, bath, ',');
         getline(iss, city, ',');
@@ -35,23 +41,26 @@ int main() {
         houses.push_back(temp);
         Skip.insert(city, stoi(price), stoi(bed), stoi(bath), stoi(zip_code));
     }
+    bed_filter = 0;
+    bath_filter = 0;
+    zip_filter = 0;
     while (running) {
         std::cout <<"There are " << houses.size() << " houses available." << std::endl;
         std::cout << "Current Filters: " << std::endl;
-        std::cout << "City: " << (city_filter == "" ? "None" : city_filter) << std::endl;
+        std::cout << "City: " << (city_filter == "" ? "Any City" : city_filter) << std::endl;
         std::cout << "Beds: " << (bed_filter == 0 ? "Any" : to_string(bed_filter)) << std::endl;
         std::cout << "Baths: " << (bath_filter == 0 ? "Any" : to_string(bath_filter)) << std::endl;
         std::cout << "Zip Code: " << (zip_filter == 0 ? "Any" : to_string(zip_filter)) << std::endl;    
-        std::cout << "0: Exit" << std::endl;
         std::cout << "1: Set City" << std::endl;
         std::cout << "2: Set Beds" << std::endl;
         std::cout << "3: Set Baths" << std::endl;
         std::cout << "4: Set Zip Code" << std::endl;
         std::cout << "5: View Top 5 Cheapest Houses" << std::endl;
+        std::cout << "6: Exit" << std::endl;
         std::cout << "Select an option: ";
         int option;
         std::cin >> option;
-        if (option == 0){running = false;}
+     
         if (option == 1){
             std::cout << "Enter City Name: ";
             std::cin >> city_filter;
@@ -69,10 +78,31 @@ int main() {
             std::cin >> zip_filter;
         }
         if (option == 5){
-            auto cheapestHouses = Skip.getCheapest(5);
-            for (const auto& house : cheapestHouses) {
-                std::cout << "Price: " << house.price << ", Beds: " << house.beds << ", Baths: " << house.baths << ", City: " << house.city << ", Zip Code: " << house.zip_code << std::endl;
+            std::cout << "Which data structure would you like to use?" << std::endl;
+            std::cout << "1: Skip List" << std::endl;
+            std::cout << "2: Heap" << std::endl;
+            int ds_option;
+            std::cin >> ds_option;
+            if (ds_option == 1){
+                auto cheapest = Skip.getCheapest(5);
+                std::cout << "Top 5 Cheapest Houses:" << std::endl;
+                cout << "House 1: Price: $" << cheapest[0].price << ", Beds: " << cheapest[0].beds << ", Baths: " << cheapest[0].baths << ", City: " << cheapest[0].city << ", Zip Code: " << cheapest[0].zip_code << endl;
+                cout << "House 2: Price: $" << cheapest[1].price << ", Beds: " << cheapest[1].beds << ", Baths: " << cheapest[1].baths << ", City: " << cheapest[1].city << ", Zip Code: " << cheapest[1].zip_code << endl;
+                cout << "House 3: Price: $" << cheapest[2].price << ", Beds: " << cheapest[2].beds << ", Baths: " << cheapest[2].baths << ", City: " << cheapest[2].city << ", Zip Code: " << cheapest[2].zip_code << endl;
+                cout << "House 4: Price: $" << cheapest[3].price << ", Beds: " << cheapest[3].beds << ", Baths: " << cheapest[3].baths << ", City: " << cheapest[3].city << ", Zip Code: " << cheapest[3].zip_code << endl;
+                cout << "House 5: Price: $" << cheapest[4].price << ", Beds: " << cheapest[4].beds << ", Baths: " << cheapest[4].baths << ", City: " << cheapest[4].city << ", Zip Code: " << cheapest[4].zip_code << endl;
             }
+            cout << "1: Go Back to Main Menu" << endl;
+            int back_option;
+            std::cin >> back_option;
+            system("cls");
+            
         }
+        if (option == 6){
+            Skip.~SkipList();
+            running = false;
+        }
+        option = 0;
     }
+    return 0;
 }
